1c914c6268de8fcec5f6990814daff6ecf98c356
morsinska <augosa86@gmail.com>
2026-05-03 13:50:31 +0300

    Gamesense comment correct finaly

--- tasks/models.py
+++ tasks/models.py
@@ -34,11 +34,12 @@ class Task(models.Model):
 
 
 class Comment(models.Model):
+    # Добавь related_name='comments' в конец этой строки:
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    media = models.FileField(upload_to="comments_media/", blank=True, null=True)
+
 
     def get_absolute_url(self):
         return self.task.get_absolute_url()
